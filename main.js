3d213454f90f47353341640c2479aece6c26e736
Jan Zaloudek <honzicekz@gmail.com>
2014-03-30 15:05:01 +0200

    basic features

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ var Parser = require('./parser');
 
 commander
   .version(package.version)
-  .usage('[options] <file ...>')    
+  .usage('[options] <file ...>')
+  .option('-p, --pedantic', 'Pedantic mode is checking for inconsistency in map', true)
+  .option('-f, --foreign-keys', 'Disable foreign keys')
+  .option('-l, --leave-content', 'Does not truncate tables')
   .parse(process.argv);
 
 if(commander.args.length === 0) {
@@ -16,8 +19,9 @@ if(commander.args.length === 0) {
 }
 
 commander.args.forEach(function(file) {
-  var parser = new Parser(file);
-  parser.parse();  
+  var parser = new Parser(file, commander);
+  
+  process.stdout.write(parser.parse());
 });
 
 
--- parser.js
+++ parser.js
@@ -10,7 +10,19 @@ var Parser = function(file_name, options) {
 }
 
 Parser.prototype.getEdgesQueries_ = function() {
+  if(!this.dom_) {
+    throw new Error('XML DOM is not loaded');
+  }
 
+  var queries = [];
+  var self = this;
+  var counter = 0;
+
+  this.dom_('nodes node edge').each(function(index, node) {
+    queries.push('INSERT INTO `edge` (`id`, `from_id`, `to_id`) VALUES ('+(++counter)+', '+this.attr('fromid')+', '+this.attr('toid')+');');
+  });
+
+  return queries;
 }
 
 Parser.prototype.getNodesQueries_ = function() {
@@ -21,9 +33,11 @@ Parser.prototype.getNodesQueries_ = function() {
   var queries = [];
   var self = this;
 
-  // this.dom_('nodes').each(function(index, node) {
-  //   console.log(self.dom_('edge', this).toArray());
-  // });
+  this.dom_('nodes node').each(function(index, node) {
+    queries.push('INSERT INTO `node` (`id`) VALUES ('+this.attr('id')+');');
+  });
+
+  return queries;
 }
 
 Parser.prototype.parse = function() {
@@ -40,7 +54,35 @@ Parser.prototype.parse = function() {
   var nodes_queries = this.getNodesQueries_() || [];
   var edges_queries = this.getEdgesQueries_() || [];
 
-  var result 
+  var result = '';
+
+  result += '-- Nodes for ZUMScore --\n\n';
+  result += '-- Source: '+this.file_name+'\n';
+  result += '-- Generated at: '+(new Date().toUTCString())+'\n';
+  result += '-- Repository: https://github.com/fitak/zum-nodes-parser.git'+'\n';
+  result += '\n';
+
+  if(this.options['foreignKeys']) {
+    result += 'SET FOREIGN_KEY_CHECKS=0;\n';
+  }
+
+  if(!this.options['leaveContent']) {
+    result += 'TRUNCATE edge;\n'
+    result += 'TRUNCATE node;\n';  
+  }
+
+  result += '\n';
+
+  result += '-- Generating nodes\n';
+  result += nodes_queries.join('\n');
+  result += '\n\n';
+
+  result += '-- Generating edges\n';
+  result += edges_queries.join('\n');
+
+  result += '\n\n-- End of '+this.file_name+'\n';
+
+  return result;
 }
 
 module.exports = Parser;
